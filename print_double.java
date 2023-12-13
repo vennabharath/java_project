0ebb388a39e20154e5aa0b7d8e5752d1239e0fd9
bharathvenna <bharathvenna.venna@gmail.com>
2023-12-13 22:08:17 +0530

    print double added

--- print_double.java
+++ print_double.java
@@ -1,11 +1,21 @@
-public class print_double {
-    Scanner input = new Scanner(System.in);
-    double[] numbers = new double[5];
+import java.util.Scanner;
 
-    for(
-    int i = 0;i<numbers.length;i++)
-    {
-        System.out.println("Please enter number");
-        numbers[i] = input.nextDouble();
+public class print_double {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double[] numbers = new double[5];
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Please enter the numbers");
+            numbers[i] = input.nextDouble();
+        }
+        print_num(numbers);
+    }
+
+    public static void print_num(double[] numbers) {
+        System.out.println("The array contains");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(+numbers[i]);
+        }
     }
 }
--- single_inheritance.java
+++ single_inheritance.java
@@ -1,16 +1,17 @@
 public class single_inheritance {
     int a = 0;
     int b = 10;
+
 }
 
 class A extends SingleInheritance {
 
     int c = 20;
+    int sum = (a+b+c)
 
-    @Override
     public void main(String[] args) {
-        System.out.println("A");
-        System.err.println(a, "/t", b, "/t", c);
+
+        System.out.println(a, "/t", b, "/t", c);
     }
 
 }
